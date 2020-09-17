b62663323322c9d0ec8c6b5f6b328c7263192e2a
Caio <caiobernardodasilva@gmail.com>
2020-09-17 01:57:10 -0300

    sdfsdf

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const PORT = process.env.PORT || 5000
 const app = express();
 
 app.use(bodyParser.json());
@@ -8,4 +8,4 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 require('./app/controllers/index')(app);
 
-app.listen(3000);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
