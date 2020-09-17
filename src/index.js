e5a4588828f5d873f764fab04503f1e0bd30faa2
Caio <caiobernardodasilva@gmail.com>
2020-09-17 02:13:36 -0300

    Localhost:3000

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000
+//ON heroku
+//const PORT = process.env.PORT || 5000 
 const app = express();
 
 app.use(bodyParser.json());
@@ -8,4 +9,6 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 require('./app/controllers/index')(app);
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(3000);// app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
